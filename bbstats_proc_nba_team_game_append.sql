11043a830267d3e79101bf76c3824e643758d4c5
eltrungo <82531813+eltrungo@users.noreply.github.com>
2021-05-06 10:49:18 -0700

    Update bbstats_proc_nba_team_game_append.sql

--- bbstats_proc_nba_team_game_append.sql
+++ bbstats_proc_nba_team_game_append.sql
@@ -11,12 +11,11 @@ INSERT INTO `nba_team_game`
 		`Result`,
 		count(`NBA_playerId`),
 		NULL,NULL,NULL,
-        NULL,NULL,NULL,NULL
-    
+        	NULL,NULL,NULL,NULL
 	FROM `nba_player_game` AS npg
-    WHERE npg.`Game_Date` = (`max_date_nba_team_game`() + INTERVAL 1 DAY)
-    GROUP BY `Team`, `Away`, `Oppo`, `Game_Date`, `Result`
-    ORDER BY `Game_Date` ASC, `Team` ASC;
+    	WHERE npg.`Game_Date` = (`max_date_nba_team_game`() + INTERVAL 1 DAY)
+    	GROUP BY `Team`, `Away`, `Oppo`, `Game_Date`, `Result`
+    	ORDER BY `Game_Date` ASC, `Team` ASC;
 
 UPDATE `nba_team_game`
 	SET `Team_Game` = if(`Away` = "",
@@ -29,38 +28,37 @@ UPDATE `nba_team_game`
 
 UPDATE `nba_team_game` AS ntg
     LEFT JOIN 
-		(SELECT
-			a.`team_game_id`,
-            COUNT(b.`team_game_id`)+1 AS `team_game_seq`
-			FROM `nba_team_game` AS a
-				JOIN `nba_team_game` AS b
-                ON a.`Team` = b.`Team` 
-					AND (a.`Game_Date` > b.`Game_Date` 
-					OR (a.`Game_Date` = b.`Game_Date` AND a.`team_game_id` > b.`team_game_id`))
+	(SELECT a.`team_game_id`,
+            	COUNT(b.`team_game_id`)+1 AS `team_game_seq`
+		FROM `nba_team_game` AS a
+			JOIN `nba_team_game` AS b
+                	ON a.`Team` = b.`Team` 
+				AND (a.`Game_Date` > b.`Game_Date` 
+				OR (a.`Game_Date` = b.`Game_Date` AND a.`team_game_id` > b.`team_game_id`))
 		GROUP BY a.`team_game_id`) AS c
-    ON c.`team_game_id` = ntg.`team_game_id`    
-SET ntg.`team_game_seq` = c.`team_game_seq`;
+	ON c.`team_game_id` = ntg.`team_game_id`    
+	SET ntg.`team_game_seq` = c.`team_game_seq`;
 
 UPDATE `nba_team_game`
 	SET `team_game_seq` = '1'
-    WHERE `team_game_seq` IS NULL;
+	WHERE `team_game_seq` IS NULL;
 
 UPDATE `nba_team_game`
 	SET
 		`days_gm_before` = NULL,
 		`days_gm_after` = NULL,
-        `B2B_Day` = NULL;
+        	`B2B_Day` = NULL;
 
 UPDATE `nba_team_game` AS ntg
 	INNER JOIN `nba_team_game` AS ntg2
 		ON ntg.`Team` = ntg2.`Team`
-        AND ntg.`team_game_seq` = ntg2.`team_game_seq`+ 1
+        	AND ntg.`team_game_seq` = ntg2.`team_game_seq`+ 1
 	SET ntg.`days_gm_before` = datediff(ntg.`Game_Date`, ntg2.`Game_Date`);
 
 UPDATE `nba_team_game` AS ntg
 	INNER JOIN `nba_team_game` AS ntg2
 		ON ntg.`Team` = ntg2.`Team`
-        AND ntg.`team_game_seq` = ntg2.`team_game_seq`- 1
+        	AND ntg.`team_game_seq` = ntg2.`team_game_seq`- 1
 	SET ntg.`days_gm_after` = datediff(ntg2.`Game_Date`, ntg.`Game_Date`);
 
 UPDATE `nba_team_game`
